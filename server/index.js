f209439950fed74db346819a31f794234b2f0cc9
Gerome Mico Tahud <gerome@rushmedia.ph>
2024-01-19 15:05:57 +0800

    Changed time zone for deletion to Asia/Manila

--- server/index.js
+++ server/index.js
@@ -1,76 +1,86 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const { Op } = require('sequelize');
-const db = require('./models');
-const { Users, Records, Floor } = require('./models');
+const { Op } = require("sequelize");
+const db = require("./models");
+const { Users, Records, Floor } = require("./models");
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 5000;
 
 //routers
-const recordRouter = require('./routes/Record');
+const recordRouter = require("./routes/Record");
 app.use("/record", recordRouter);
 
 const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
 
-const floorRouter = require('./routes/Floor');
+const floorRouter = require("./routes/Floor");
 app.use("/floor", floorRouter);
 
-const studentsRouter = require('./routes/Students');
+const studentsRouter = require("./routes/Students");
 app.use("/student", studentsRouter);
 
-const reservationRouter = require('./routes/Reservation');
+const reservationRouter = require("./routes/Reservation");
 app.use("/reservation", reservationRouter);
 
-const confabRouter = require('./routes/Confab');
+const confabRouter = require("./routes/Confab");
 app.use("/confab", confabRouter);
 
 db.sequelize.sync().then(async () => {
-    const adminUser = await Users.findOne({ where: { type: 'Admin' } });
+    const adminUser = await Users.findOne({ where: { type: "Admin" } });
 
     if (!adminUser) {
         // Create admin user
         Users.create({
-            name: 'Admin',
-            username: 'superAdmin',
-            password: 'capstone2023',
-            type: 'Admin'
-        }).then(() => {
-            console.log('Admin user created');
-        }).catch(error => {
-            console.error('Error creating admin user:', error);
-        });
+            name: "Admin",
+            username: "superAdmin",
+            password: "capstone2023",
+            type: "Admin",
+        })
+            .then(() => {
+                console.log("Admin user created");
+            })
+            .catch((error) => {
+                console.error("Error creating admin user:", error);
+            });
     }
 
     const currentDate = new Date();
 
+    const options = { timeZone: "Asia/Manila" };
+    const philippineDate = currentDate.toLocaleString("en-US", options).slice(0, 10);
+
     await Records.destroy({
         where: {
             date: {
-                [Op.lt]: currentDate.toISOString().slice(0, 10),
+                [Op.lt]: philippineDate,
             },
-            time_out: null
-        }
+            time_out: null,
+        },
     })
-        .then(deletedCount => {
+        .then((deletedCount) => {
             console.log(`${deletedCount} records deleted successfully.`);
             if (deletedCount > 0) {
                 Floor.update({ current_count: 0 }, { where: {} })
-                    .then(updatedCount => {
-                        console.log(`${updatedCount} instances updated successfully.`);
+                    .then((updatedCount) => {
+                        console.log(
+                            `${updatedCount} instances updated successfully.`
+                        );
                     })
-                    .catch(err => {
-                        console.error('Error occurred while updating instances:', err);
+                    .catch((err) => {
+                        console.error(
+                            "Error occurred while updating instances:",
+                            err
+                        );
                     });
             }
         })
-        .catch(err => {
-            console.error('Error occurred while deleting records:', err);
+        .catch((err) => {
+            console.error("Error occurred while deleting records:", err);
         });
     app.listen(port, () => {
-        console.log(`🚀 Server running on port ${port}`)
+        console.log(`🚀 Server running on port ${port}`);
     });
-})
+});
